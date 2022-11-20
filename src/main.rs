71a98e22389116947123d3163c9510d6b2a95486
jacob l <jacobinmt47@outlook.com>
2022-11-20 11:01:53 -0700

    got gui running

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,18 @@ use nwg::NativeUi;
 
 #[derive(Default, NwgUi)]
 pub struct BasicApp {
-    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_control(size: (300, 200), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
     window: nwg::Window,
 
-    #[nwg_control(text: "Heisenberg", size: (280, 25), position: (10, 10))]
-    name_edit: nwg::TextInput,
+    #[nwg_control(text: "number of stations", size: (280, 25), position: (10, 10))]
+    station_edit: nwg::TextInput,
+    #[nwg_control(text: "length", size: (280, 25), position: (10,45))]
+    length_edit: nwg::TextInput,
+    #[nwg_control(text: "Trend", size: (280, 25), position: (10,80))]
+    trend_edit: nwg::TextInput,
 
-    #[nwg_control(text: "Say my name", size: (280, 60), position: (10, 40))]
+    #[nwg_control(text: "calculate", size: (280, 60), position: (10, 120))]
     #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
     hello_button: nwg::Button
 }
@@ -28,11 +32,11 @@ pub struct BasicApp {
 impl BasicApp {
 
     fn say_hello(&self) {
-        nwg::simple_message("Hello", &format!("Hello {}", self.name_edit.text()));
+       
     }
     
     fn say_goodbye(&self) {
-        nwg::simple_message("Goodbye", &format!("Goodbye {}", self.name_edit.text()));
+        nwg::simple_message("{}","Goodbye");
         nwg::stop_thread_dispatch();
     }
 
