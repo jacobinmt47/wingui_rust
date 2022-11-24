f0a7aaef14c0860e5e1f40db0c06728859c1a0c5
jacob l <jacobinmt47@outlook.com>
2022-11-24 14:23:47 -0700

    add labels to window

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,24 @@ use rand_distr::{Normal, Distribution};
 
 #[derive(Default, NwgUi)]
 pub struct BasicApp {
-    #[nwg_control(size: (300, 200), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_control(size: (300, 200), position: (300, 300), title: "lows at end per trend", flags: "WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
     window: nwg::Window,
 
-    #[nwg_control(text: "number of stations", size: (280, 25), position: (10, 10))]
+    #[nwg_control(text: "1000", size: (100, 25), position: (150, 10))]
     station_edit: nwg::TextInput,
-    #[nwg_control(text: "length", size: (280, 25), position: (10,45))]
+
+    #[nwg_control(text: "number of stations",position:(10,10))]
+    station_label: nwg::Label,
+
+    #[nwg_control(text: "100", size: (100, 25), position: (150,45))]
     length_edit: nwg::TextInput,
-    #[nwg_control(text: "Trend per year", size: (280, 25), position: (10,80))]
+    #[nwg_control(text: "length run in years",position:(10,45))]
+    length_label: nwg::Label,
+    #[nwg_control(text: "0.003", size: (100, 25), position: (150,80))]
     trend_edit: nwg::TextInput,
+    #[nwg_control(text: "Trend per year",position:(10,80))]
+    trend_label: nwg::Label,
 
     #[nwg_control(text: "calculate", size: (280, 60), position: (10, 120))]
     #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
